4776c6ee8d4e57f69329c6e3ce9d68821f16727a
fatih <rinqtf@gmail.com>
2025-05-08 23:06:52 +0300

    get word count from a book and print the count

---
+++ main.py
@@ -0,0 +1,22 @@
+def get_book_text(filepath: str) -> str:
+    file_contents = ""
+    with open(filepath) as f:
+        file_contents = f.read()
+
+    return file_contents
+
+
+def count_words(text: str) -> int:
+    words = text.split()
+
+    return len(words)
+
+
+def main() -> None:
+    filepath = "./books/frankenstein.txt"
+    book_text = get_book_text(filepath)
+    num_words = count_words(book_text)
+    print(f"{num_words} words found in the document")
+
+
+main()
