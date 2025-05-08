222d5310b45bdd6c138fcbe38b8e3fd3e197b54d
fatih <rinqtf@gmail.com>
2025-05-08 23:23:07 +0300

    print dictionary as well

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import count_words
+from stats import count_words, count_characters
 
 
 def get_book_text(filepath: str) -> str:
@@ -13,7 +13,9 @@ def main() -> None:
     filepath = "./books/frankenstein.txt"
     book_text = get_book_text(filepath)
     num_words = count_words(book_text)
+    characters = count_characters(book_text)
     print(f"{num_words} words found in the document")
+    print(characters)
 
 
 main()
--- stats.py
+++ stats.py
@@ -2,3 +2,15 @@ def count_words(text: str) -> int:
     words = text.split()
 
     return len(words)
+
+
+def count_characters(text: str) -> dict[str, int]:
+    characters: dict[str, int] = {}
+
+    for c in text:
+        if c.lower() in characters:
+            characters[c.lower()] += 1
+        else:
+            characters[c.lower()] = 1
+
+    return characters
